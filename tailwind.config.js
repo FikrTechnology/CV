ee6638450ee234edf7ad9bd3f315b44b9a467854
Fikrie <Muhammad.Fikrie@bni.co.id>
2022-08-24 15:30:01 +0700

    Create Portfolio Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{html,js}"],
+  content: ["index.html"],
   theme: {
     container: {
       center: true,
